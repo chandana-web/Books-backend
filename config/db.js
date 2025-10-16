b070c13fa768342355626d9f4a3cec3f2cbf433e
chandana-web <143087553+chandana-web@users.noreply.github.com>
2025-10-16 13:24:28 +0530

    fixes

--- config/db.js
+++ config/db.js
@@ -1,7 +1,10 @@
 const mongoose=require("mongoose");
 const connectDB=async()=>{
     try{
-    await mongoose.connect(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connection is established");
     }
     catch(err){
